db8cbbbcdaccb0072f9e2a9fd5a456e61a6271b4
Olaf Hartig <olaf.hartig@liu.se>
2018-06-29 12:30:27 +0200

    added library dependencies that are needed for some of the code of Jesper and Amir

--- src/test/java/se/liu/ida/rdfstar/pgtools/conversion/PG2RDFStarTest.java
+++ src/test/java/se/liu/ida/rdfstar/pgtools/conversion/PG2RDFStarTest.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.fail;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.lang3.StringUtils;
 
 import se.liu.ida.rdfstar.tools.parser.lang.LangTurtleStarTest;
 
@@ -30,6 +32,11 @@ public class PG2RDFStarTest
 	public void todoWriteTests()
 	{
 		// TODO ...
+
+		// The following two lines are just to make sure that the additional
+		// Java libraries can be used. You can delete these lines later. --Olaf 
+		final CSVFormat test = CSVFormat.RFC4180;
+		final String test2 = StringUtils.SPACE;
 
 		LangTurtleStarTest.createGraphFromTurtleStarSnippet("...");
 	}
